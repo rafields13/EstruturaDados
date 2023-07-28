3ef6e1567648bd48a9e5035c5746fdd93f441a8f
Rafael <rafaelmarliere033@gmail.com>
2023-07-27 23:34:59 -0300

    02 solved

--- exercicios/ex02.c
+++ exercicios/ex02.c
@@ -1,3 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
+
+int main(void) {
+    setlocale(LC_ALL, "Portuguese_Brazil.1252");
+
+    int n;
+
+    printf("Digite um número, por favor: ");
+    scanf("%d", &n);
+
+    if (n % 2 == 0) {
+        printf("O número %d é par!\n", n);
+    } else {
+        printf("O número %d é ímpar!\n", n);
+    }
+
+    system("pause");
+    return 0;
+}
